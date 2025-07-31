2197399c3b5d7a1600304ec19719b8ce900430c9
primehunter <primehunter575@gmail.com>
2025-07-31 20:05:19 +0200

    Creación de clase Rey

--- src/modelo/Rey.java
+++ src/modelo/Rey.java
@@ -1,5 +1,43 @@
 package modelo;
 
-public class Rey {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rey extends Pieza {
+
+    public Rey(boolean esBlanca) {
+        super(esBlanca);
+    }
+
+    @Override
+    public List<Posicion> movimientosValidos(Tablero tablero, int x, int y) {
+        List<Posicion> movimientos = new ArrayList<>();
+
+        int[][] deltas = {
+            {0, 1},   // arriba
+            {0, -1},  // abajo
+            {1, 0},   // derecha
+            {-1, 0},  // izquierda
+            {1, 1},   // arriba-derecha
+            {1, -1},  // abajo-derecha
+            {-1, 1},  // arriba-izquierda
+            {-1, -1}  // abajo-izquierda
+        };
+
+        for (int[] d : deltas) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+            Posicion destino = new Posicion(nx, ny);
+            if (!destino.estaDentroDelTablero()) continue;
+
+            Pieza ocupante = tablero.obtenerPieza(nx, ny);
+            if (ocupante == null || ocupante.esBlanca() != this.esBlanca()) {
+                movimientos.add(destino);
+            }
+        }
+
+        // Nota: el enroque y la verificación de si el rey quedaría en jaque se implementan después.
+
+        return movimientos;
+    }
 }
